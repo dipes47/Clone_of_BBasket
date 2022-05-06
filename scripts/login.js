b55c8e7fa6eb06cf9ec2c43c2a66f8ca760bc6b9
dipes47 <dipes94@gmail.com>
2022-05-06 21:13:54 +0530

    c8

---
+++ scripts/login.js
@@ -0,0 +1,42 @@
+
+  async function loginData() {
+    try{
+      let id = document.querySelector("#username").value;
+      let pass = document.querySelector("#password").value;
+      let mail = {email: id,
+                  password: pass}
+                let response = await fetch("https://bigbasketnewdb.herokuapp.com/login",{
+                method:"POST",
+                body:JSON.stringify(mail),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            let data = await response.json()
+            console.log(data.user)
+            localStorage.setItem("UserInfo",JSON.stringify(data))
+            // nameApnd()
+            if(data.message){
+              
+              alert(data.message)
+            }
+            else{
+              alert("Successfully Logged In ")
+              window.location.href="../products.html"
+            }
+    }catch(err){
+        console.log(err.message)
+    }
+  }
+
+//     function nameApnd(){
+//       let userData = JSON.parse(localStorage.getItem("UserInfo")) || null;
+//       if(userData){
+//         let uName = document.getElementById("loginbutton");
+//         uName.innerHTML = userData.user.name;
+//       }
+//   }
+
+function loginWithGoogle(){
+  window.location.href="https://bigbasketnewdb.herokuapp.com/auth/google"
+}
---
+++ scripts/signup.js
@@ -0,0 +1,29 @@
+// function registerUser(){
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value;
+//     let password = document.getElementById("password").value;
+// }
+
+async function registerUser() {
+    try{
+            let mail = {
+                        name: document.getElementById("name").value,
+                        email:document.querySelector("#email").value,
+                        password: document.getElementById("password").value
+                      };
+            let response = await fetch("https://bigbasketnewdb.herokuapp.com/register",{
+                method:"POST",
+                body:JSON.stringify(mail),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            let data = await response.json()
+            if(data){
+                alert("Registration Successfull!!");
+                window.location.href="../login.html"
+            }
+        } catch(err){
+            console.log(err.message)
+        }
+  }
